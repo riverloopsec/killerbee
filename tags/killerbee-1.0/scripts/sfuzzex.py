22cdecef78331710f1aaa66941156dc0e10abf90
rmspeers <rmspeers@8c04375a-fa35-11de-8173-5df4dc7aba59>
2011-03-12 15:13:38 +0000

    tagging josh's initial killerbee release as 1.0

---
+++ tags/killerbee-1.0/scripts/sfuzzex.py
@@ -0,0 +1,27 @@
+# This is a simple example of integrating Sulley for packet mutation with
+# KillerBee as the delivery mechanism.  Sulley is a lot more sophisticated and
+# can do a lot more interesting mutation than this simple example, but it
+# should be enough to get you going.  I'm still working on a reliable "ping"
+# function to measure the availability of the target.  -Josh
+import sys
+sys.path.append("/home/jwright/fuzzing/sulley")
+from killerbee import *
+from sulley import *
+
+dstpan = "\xff\xff"
+dst = "\xff\xff"
+src = "\x00\x0d\x6f\x04\x00\x00\x00\x50"
+
+s_initialize("MAC Command Mutation")
+s_static("\x43\xc8\x00") # FC + Seq#
+s_static(dstpan[::-1]) # Addresses are specified in normal order, then reversed
+s_static(dst[::-1])
+s_static(src[::-1])
+s_byte(0, full_range=True)
+
+kb = KillerBee()
+kb.set_channel(26)
+
+while s_mutate():
+    kb.inject(s_render())
+    print hexdump(s_render())
---
+++ tags/killerbee-1.0/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup, Extension
+import sys
+
+err = ""
+
+try:
+    import gtk
+except ImportError:
+    err += "gtk (apt-get install python-gtk2)\n"
+
+try:
+    import cairo
+except ImportError:
+    err += "cairo (apt-get install python-cairo)\n"
+
+try:
+    import Crypto
+except ImportError:
+    err += "crypto (apt-get install python-crypto)\n"
+
+try:
+    import usb
+except ImportError:
+    err += "usb (apt-get install python-usb)\n"
+
+if err != "":
+    print >>sys.stderr, """
+Library requirements not met.  Install the following libraries, then re-run
+the setup script.
+
+    """, err
+    sys.exit(1)
+    
+
+setup  (name        = 'killerbee',
+        version     = '1.0',
+        description = 'ZigBee and IEEE 802.15.4 Attack Framework and Tools',
+        author = 'Joshua Wright',
+        author_email = 'jwright@willhackforsushi.com',
+        packages  = ['killerbee'],
+        requires = ['Crypto', 'usb', 'gtk', 'cairo'], # Not causing setup to fail, not sure why
+        scripts = ['tools/zbdump', 'tools/zbgoodfind', 'tools/zbid', 'tools/zbreplay', 'tools/zbconvert', 'tools/zbdsniff', 'tools/zbstumbler', 'tools/zbassocflood', 'tools/zbfind']
+        )
