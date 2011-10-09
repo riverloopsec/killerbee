fb462010e3e618bc9fb01a5387ce6788ac910bb4
melgares <melgares@8c04375a-fa35-11de-8173-5df4dc7aba59>
2011-10-09 02:19:01 +0000

    added support for logging of location information to PCAP using the PPI geolocation standard

--- killerbee/killerbee/pcapdump.py
+++ killerbee/killerbee/pcapdump.py
@@ -4,9 +4,12 @@ import time
 PCAPH_MAGIC_NUM = 0xa1b2c3d4
 PCAPH_VER_MAJOR = 2
 PCAPH_VER_MINOR = 4
-PCAPH_THISZONE = 0
-PCAPH_SIGFIGS = 0
-PCAPH_SNAPLEN = 65535
+PCAPH_THISZONE  = 0
+PCAPH_SIGFIGS   = 0
+PCAPH_SNAPLEN   = 65535
+
+DOT11COMMON_TAG = 000002
+GPS_TAG		= 30002
 
 class PcapReader:
 
@@ -125,7 +128,7 @@ class PcapDumper:
             ]))
 
     def pcap_dump(self, packet, ts_sec=None, ts_usec=None, orig_len=None, 
-                  freq_mhz = None, ant_dbm = None):
+                  freq_mhz = None, ant_dbm = None, location = None):
         '''
         Appends a new packet to the libpcap file.  Optionally specify ts_sec
         and tv_usec for timestamp information, otherwise the current time is
@@ -140,6 +143,8 @@ class PcapDumper:
         @param orig_len: Length of the original packet, used if the packet you
         are writing is smaller than the original packet.  Defaults to the
         specified packet's length.
+        @type location: Tuple
+        @param location: 3-tuple of (longitude, latitude, altitude).
         @type packet: String
         @param packet: Packet contents
         @rtype: None
@@ -147,33 +152,64 @@ class PcapDumper:
 
         # Build CACE PPI headers if requested
         if self.ppi is True:
-            pph_len = 8 + 24 #ppi_header + 802.11-common header and data
+            pph_len = 8 #ppi_header
+
+            #CACE PPI Field 802.11-Common
+            pph_len += 28 #802.11-common header and data
             rf_freq_mhz = 0x0000
             if freq_mhz is not None: rf_freq_mhz = freq_mhz
             rf_ant_dbm = 0
             if ant_dbm is not None: rf_ant_dbm = ant_dbm
+            caceppi_f80211common = ''.join([
+                struct.pack("<H", DOT11COMMON_TAG), #2 = Field Type 802.11-Common
+                struct.pack("<H", 20),              #20 = 802.11-Common length in bytes
+                struct.pack("<Q", 0),               #FSF-Timer
+                struct.pack("<H", 0),               #Flags
+                struct.pack("<H", 0),               #Rate
+                struct.pack("<H", rf_freq_mhz),     #Channel-Freq
+                struct.pack("<H", 0x0080),          #Channel-Flags = 2GHz
+                struct.pack("<B", 0),               #FHSS-Hopset
+                struct.pack("<B", 0),               #FHSS-Pattern
+                struct.pack("<b", rf_ant_dbm),      #dBm-Ansignal
+                struct.pack("<b", 0)                #dBm-Antnoise
+                ])
+
+            #PPI-GEOLOCATION if information available
+            if location is not None:
+                pph_len += 20 #geolocation header and data length
+                (lon, lat, alt) = location
+                # Sanity checking on values of location data:
+                if lat > -180.00000005 and lat < 180.00000005:
+                    lat_i = int(round((lat + 180.0) * 1e7))
+                else:
+                    raise Exception("Latitude value is out of expected range: %.8f" % lat)
+                if lon > -180.00000005 and lon < 180.00000005:
+                    lon_i = int(round((lon + 180.0) * 1e7))
+                else:
+                    raise Exception("Longitude value is out of expected range: %.8f" % lon)
+                if alt > -180000.00005 and alt < 180000.00005:
+                    alt_i = int(round((alt + 180000.0) * 1e4))
+                else:
+                    raise Exception("Altitude value is out of expected range: %.8f" % lon)
+                # Build Geolocation PPI Header
+                caceppi_fgeolocation = ''.join([
+                    struct.pack("<H", GPS_TAG),  #2 = Field Type 802.11-Common
+                    struct.pack("<H", 20),       #20 = 802.11-Common length in bytes
+                    struct.pack("<B", 1),        #Geotag Version
+                    struct.pack("<B", 2),        #Geotag Pad
+                    struct.pack("<H", 24),       #Geotag Length
+                    struct.pack("<I", 0x0E),     #GPS fields (only lat, long, and alt for now)
+                    struct.pack("<I", lat_i),    #GPS Latitude
+                    struct.pack("<I", lon_i),    #GPS Longitude
+                    struct.pack("<I", alt_i),    #GPS Altitude
+                    ])
 
             #CACE PPI Header
             caceppi_hdr = ''.join([
-                struct.pack("<B", 0),
-                struct.pack("<B", 0x01),
-                struct.pack("<H", pph_len),
-                struct.pack("<I", self.datalink)
-                ])
-
-            #CACE PPI Field 802.11-Common
-            caceppi_f80211common = ''.join([
-                struct.pack("<H", 2),        #2 = Field Type 802.11-Common
-                struct.pack("<H", 20),       #20 = 802.11-Common length in bytes
-                struct.pack("<Q", 0),        #FSF-Timer
-                struct.pack("<H", 0),        #Flags
-                struct.pack("<H", 0),        #Rate
-                struct.pack("<H", rf_freq_mhz), #Channel-Freq
-                struct.pack("<H", 0x0080),   #Channel-Flags = 2GHz
-                struct.pack("<B", 0),        #FHSS-Hopset
-                struct.pack("<B", 0),        #FHSS-Pattern
-                struct.pack("<b", rf_ant_dbm),  #dBm-Ansignal
-                struct.pack("<b", 0)         #dBm-Antnoise
+                struct.pack("<B", 0),		     #PPH version
+                struct.pack("<B", 0x00),         #PPH flags
+                struct.pack("<H", pph_len),	     #PPH len
+                struct.pack("<I", self.datalink) #Field
                 ])
 
         if ts_sec == None or ts_usec == None:
@@ -196,6 +232,8 @@ class PcapDumper:
             output_list[2] = struct.pack("I", orig_len + pph_len)
             output_list[3] = struct.pack("I", plen + pph_len)
             output_list.append(caceppi_hdr)
+            if location is not None:
+                output_list.append(caceppi_fgeolocation)
             output_list.append(caceppi_f80211common)
 
         output_list.append(packet)
