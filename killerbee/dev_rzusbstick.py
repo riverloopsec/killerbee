6e78ef17f5f391d069f04016b9fc957b9d0f4252
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2015-08-02 20:57:47 -0400

    remove extra arguments to dev.read()
    pyusb 1.0.0b1 has the 'interface' argument, but 1.0.0b2 (and later) do
    not. Simply remove the extra arguments (interface and timeout).
    
    This is not an ideal fix, but makes this call consistent with all others
    that did this to avoid the pyusb arguments problem. A better solution
    might be to create our own 'kbusb' class that abstracts away all of
    these pyusb problems.
    
    See #37.
    
    Signed-off-by: Jonathon Reinhart <Jonathon.Reinhart@gmail.com>

--- killerbee/dev_rzusbstick.py
+++ killerbee/dev_rzusbstick.py
@@ -272,7 +272,7 @@ class RZUSBSTICK:
             if len(data) != res:
                 raise Exception("Issue writing USB data {0} to endpoint {1}, got a return of {2}.".format(data, endpoint, res))
             try:
-                response = self.dev.read(RZ_USB_RESPONSE_EP, self.dev.bMaxPacketSize0, 0, 500)
+                response = self.dev.read(RZ_USB_RESPONSE_EP, self.dev.bMaxPacketSize0) #, 0, 500)
                 response = response.pop()
             except usb.core.USBError as e:
                 if e.errno != 110: #Not Operation timed out
