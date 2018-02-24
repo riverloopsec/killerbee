9d547fcc5f8a2d84c4182529c9aa9fbe7822dafa
rmspeers <ryan@riverloopsecurity.com>
2018-02-24 17:19:25 -0500

    Cleaned up setup.py feedback on packages.

--- setup.py
+++ setup.py
@@ -7,67 +7,74 @@ except ImportError:
     from distutils.core import setup, Extension
 import sys
 
-err = ""
-warn = ""
+err = []
+warn = []
 
 # We have made gtk, cairo, scapy-com into optional libraries
 try:
     import gtk
 except ImportError:
-    warn += "gtk (apt-get install python-gtk2)\n"
+    warn.append(("gtk", "python-gtk2", ""))
+    # TODO: Add pip name
 
 try:
     import cairo
 except ImportError:
-    warn += "cairo (apt-get install python-cairo)\n"
+    warn.append(("cairo", "python-cairo", ""))
+    # TODO: Add pip name
 
 try:
     import Crypto
 except ImportError:
-    err += "crypto (apt-get install python-crypto)\n"
-    #pip install pycrypto
+    err.append(("crypto", "python-crypto", "pycrypto"))
 
 # Ensure we have either pyUSB 0.x or pyUSB 1.x, but we now
 #  prefer pyUSB 1.x moving forward. Support for 0.x may be deprecated.
 try:
     import usb
 except ImportError:
-    err += "usb (apt-get install python-usb)\n"
-    #pip install pyusb
+    err.append(("usb", "python-usb", "pyusb"))
 
 try:
     import usb.core
     #print("Warning: You are using pyUSB 1.x, support is in beta.")
 except ImportError:
-    warn += "You are using pyUSB 0.x. Consider upgrading to pyUSB 1.x."
+    warn.append(("NOTE: You are using pyUSB 0.x. Consider upgrading to pyUSB 1.x.", "", ""))
+    # TODO: Add apt/pip names
 
 try:
     import serial
 except ImportError:
-    err += "serial (apt-get install python-serial)\n"
+    err.append(("serial", "python-serial", "pyserial"))
 
 # Dot15d4 is a dep of some of the newer tools
 try:
     from scapy.all import Dot15d4
 except ImportError:
-    warn += "Scapy-com 802.15.4 (git clone https://bitbucket.org/secdev/scapy-com)"
+    warn.append(("scapy-com with dot15d4.py", "", ""))
 
 
-if err != "":
+if len(err) > 0:
     print("""
-Library requirements not met.  Install the following libraries, then re-run
-the setup script.
-
-    """, err, file=sys.stderr)
+Library requirements were not met.
+Install the following libraries, then re-run the setup script.
+\n\t{}\n\n
+apt-get install {}
+pip install {}""".format("\t".join(map(lambda x: x[0], err)),
+                         " ".join(map(lambda x: x[1], err)),
+                         " ".join(map(lambda x: x[2], err))), file=sys.stderr)
     sys.exit(1)
 
-if warn != "":
+if len(warn) > 0:
     print("""
-Library recommendations not met. For full support, install the following libraries,
-then re-run the setup script.
-
-    """, warn, file=sys.stderr)
-#TODO: Offer the user to type y/n to continue or cancel at this point
+Library recommendations were not met.
+For full support, install the following libraries, then re-run the setup script.
+\n\t{}\n\n
+apt-get install {}
+pip install {}""".format("\t".join(map(lambda x: x[0], warn)),
+                         " ".join(map(lambda x: x[1], warn)),
+                         " ".join(map(lambda x: x[2], warn))), file=sys.stderr)
+    # TODO: Offer the user to type y/n to continue or cancel at this point
 
 zigbee_crypt = Extension('zigbee_crypt',
                     sources = ['zigbee_crypt/zigbee_crypt.c'],
@@ -77,7 +84,7 @@ zigbee_crypt = Extension('zigbee_crypt',
                     )
 
 setup  (name        = 'killerbee',
-        version     = '2.6.0',
+        version     = '3.0.0',
         description = 'ZigBee and IEEE 802.15.4 Attack Framework and Tools',
         author = 'Joshua Wright, Ryan Speers',
         author_email = 'jwright@willhackforsushi.com, ryan@riverloopsecurity.com',
@@ -87,8 +94,8 @@ setup  (name        = 'killerbee',
         scripts = ['tools/zbdump', 'tools/zbgoodfind', 'tools/zbid', 'tools/zbreplay', 
                    'tools/zbconvert', 'tools/zbdsniff', 'tools/zbstumbler', 'tools/zbassocflood', 
                    'tools/zbfind', 'tools/zbscapy', 'tools/zbwireshark', 'tools/zbkey', 
-                    'tools/zbwardrive', 'tools/zbopenear', 'tools/zbfakebeacon', 
-                    'tools/zborphannotify', 'tools/zbpanidconflictflood', 'tools/zbrealign'],
+                   'tools/zbwardrive', 'tools/zbopenear', 'tools/zbfakebeacon',
+                   'tools/zborphannotify', 'tools/zbpanidconflictflood', 'tools/zbrealign'],
         install_requires=['pyserial>=2.0', 'pyusb', 'crypto'],
         ext_modules = [ zigbee_crypt ],
         )
