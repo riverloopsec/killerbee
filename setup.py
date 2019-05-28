ae58ca5546a1b86bf084449edc77927be780697e
rmspeers <ryan@riverloopsecurity.com>
2019-05-28 13:57:55 -0400

    Fix issue with assocPermit unpack, remove scapy git+ install from setup.py as saw sometimes issues with this syntax

--- setup.py
+++ setup.py
@@ -92,8 +92,8 @@ setup(name        = 'killerbee',
                  'tools/zbwardrive', 'tools/zbopenear', 'tools/zbfakebeacon',
                  'tools/zborphannotify', 'tools/zbpanidconflictflood', 'tools/zbrealign', 'tools/zbcat',
                  'tools/zbjammer', 'tools/kbbootloader'],
-      install_requires=['pyserial>=2.0', 'pyusb', 'pycrypto', 'rangeparser',
-                        'git+https://github.com/secdev/scapy.git#egg=scapy'],
+      install_requires=['pyserial>=2.0', 'pyusb', 'pycrypto', 'rangeparser'],
+                        #'git+https://github.com/secdev/scapy.git#egg=scapy'],
       # NOTE: pygtk doesn't install via distutils on non-Windows hosts
       ext_modules = [zigbee_crypt],
       )
