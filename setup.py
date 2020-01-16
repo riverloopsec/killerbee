9ff840b22b6eef47e6e6030354feeb97e5621c04
SteveJM <stephen.martin@royalnavymail.mod.uk>
2020-01-16 18:01:15 +0000

    Fix merge issue.

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup  (name        = 'killerbee',
                    'tools/zbwardrive', 'tools/zbopenear', 'tools/zbfakebeacon',
                    'tools/zborphannotify', 'tools/zbpanidconflictflood', 'tools/zbrealign', 'tools/zbcat', 
                    'tools/zbjammer', 'tools/kbbootloader'],
-        install_requires=['pyserial>=2.0', 'pyusb', 'pycrypto', 'rangeparser',
+        install_requires=['pyserial>=2.0', 'pyusb', 'pycrypto', 'rangeparser'],
                           #'git+https://github.com/secdev/scapy.git#egg=scapy'],
         # NOTE: pygtk doesn't install via distutils on non-Windows hosts
         ext_modules = [zigbee_crypt],
